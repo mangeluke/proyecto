109e9850137efe12f0d6a3bd520821a203b18c4a
Miguel <miguelitoguzmanpena@gmail.com>
2024-10-19 15:39:25 -0500

    actualizado2

--- app/Models/Empleado.php
+++ app/Models/Empleado.php
@@ -7,13 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Empleado extends Model
 {
-    protected $table = 'empleados';
-
     use HasFactory;
     protected $fillable=[
-        'identificacion', //del campo,del name del input
         'nombres',
-        'apellidos',
         'correo',
         'direccion',
         'telefono',
